e9aacb57299aa84fc9886cf82085d6c38876e50f
raafatfeki <fekiraafat@gmail.com>
2015-04-10 12:01:49 +0100

    The first example.

--- ptrace_exp1.c
+++ ptrace_exp1.c
@@ -1,19 +1,19 @@
+/* This code is inspired by the code written in "http://www.linuxjournal.com/article/6100" (INTEL x86_32bit system).
+ * It is rectified to support the INTEL x86_64bit system. All changes are commented and justified in diff_32_64.txt file.
+ * The basic Ptrace operation are mentioned and explained in the following link:
+ * 									"https://mikecvet.wordpress.com/2010/08/14/ptrace-tutorial/"
+ * Please see the manual page of ptrace(2) for further details.
+ * /*/
+ 
 #include <stdio.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-/* See the readme file for further details about user.h et reg.h*/
-
-#include <sys/user.h> 
+/* Please see the readme file for further details about reg.h.
+ * For further details concerning the diffrence between the x86_64bit and x86_32bit files,
+ * please see the diff_32_64.txt file.*/
 #include <sys/reg.h>
-
-/* This code is inspired by the code written in "http://www.linuxjournal.com/article/6100" (INTEL x86_32bit system).
- * It is rectified to support the INTEL x86_64bit system. All changes are commented and justified in diff_32_64.txt file.
- * The basic Ptrace operation are mentioned and explained in the following link: https://mikecvet.wordpress.com/2010/08/14/ptrace-tutorial/
- * Please see the manual page of ptrace(2) for further details.
- * /*/
  
 int main()
 {   
@@ -24,7 +24,7 @@ int main()
     {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         /* Change the execl arguments by the process you want to initiate.*/
-        execl("./helloworld_dynamic", "helloworld_dynamic", NULL);
+        execl("./helloworld_dynamic", "helloworld_dynamic", NULL);/* A sample helloworld compiled dynamically*/
     }
     else 
     {
